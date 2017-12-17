8349ce7e02e40d7de452c17f258dd04517ba686a
miyanokomiya <komiyamatomoya@mbp.local>
2017-12-17 12:15:20 +0900

    Fix bugs of delete nodes

--- src/store/nodes/mutations.js
+++ src/store/nodes/mutations.js
@@ -4,7 +4,11 @@ import { mutationTypes } from './types'
 export default {
   [mutationTypes.UPDATE_NODES] (state, { nodes }) {
     Object.keys(nodes).forEach(key => {
-      Vue.set(state.nodes, key, nodes[key])
+      if (nodes[key]) {
+        Vue.set(state.nodes, key, nodes[key])
+      } else {
+        Vue.delete(state.nodes, key)
+      }
     })
   },
   [mutationTypes.SET_SELECTED_NODES] (state, { selectedNodes }) {
