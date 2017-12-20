dbd4c170d9f6e695bd32f873eb9d7a48f9e11ff0
miyanokomiya <komiyamatomoya@mbp.local>
2017-12-21 08:18:02 +0900

    Fix warn

--- test/storybook/organisms/AuthDialog.stories.js
+++ test/storybook/organisms/AuthDialog.stories.js
@@ -2,9 +2,16 @@ import { storiesOf } from '@storybook/vue'
 
 import AuthDialog from '@/components/organisms/AuthDialog'
 
-storiesOf('organisms/AuthDialog', module)
-  .add('view', () => {
-    return {
-      render: h => h(AuthDialog)
-    }
-  })
+storiesOf('organisms/AuthDialog', module).add('view', () => {
+  return {
+    components: { AuthDialog },
+    template: `
+    <v-app>
+      <AuthDialog :value="value"/>
+    </v-app>
+    `,
+    data: () => ({
+      value: true
+    })
+  }
+})
