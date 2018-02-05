e508d32f2664decd7eab45cd6bffa721ac08b21f
miyanokomiya <komiyamatomoya@mbp.local>
2018-02-05 21:24:08 +0900

    refs #70 Ready rules and store

---
+++ src/store/comments/index.js
@@ -0,0 +1,15 @@
+import actions from './actions'
+import mutations from './mutations'
+import getters from './getters'
+
+export default {
+  namespaced: true,
+  state: {
+    fileKey: null,
+    comments: {}
+  },
+  modules: {},
+  actions,
+  mutations,
+  getters
+}
---
+++ src/store/comments/mutations.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import { mutationTypes } from './types'
+
+export default {
+  [mutationTypes.UPDATE_COMMENTS] (state, { comments }) {
+    Object.keys(comments).forEach(key => {
+      if (comments[key]) {
+        Vue.set(state.comments, key, comments[key])
+      } else {
+        Vue.delete(state.comments, key)
+      }
+    })
+  },
+  [mutationTypes.SET_FILE_KEY] (state, { fileKey }) {
+    state.fileKey = fileKey
+  },
+  [mutationTypes.CLEAR_COMMENTS] (state) {
+    state.fileKey = null
+    state.comments = {}
+  }
+}
