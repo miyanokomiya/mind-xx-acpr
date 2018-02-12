36ccebc69d02f1fdeead03bf9cca8b9326583611
miyanokomiya <komiyamatomoya@mbp-2.local>
2018-02-12 13:23:25 +0900

    Reduce flexivility of positions of nodes

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,9 @@
 export const INTERVAL_CLICK = 300
 export const INTERVAL_DOUBLE_CLICK = 300
 
-export const NODE_MARGIN_X = 20
+export const NODE_MARGIN_X = 40
 export const NODE_MARGIN_Y = 20
-export const NODE_ADDITIONAL_MARGIN_X_RATE = 0.1
+export const NODE_ADDITIONAL_MARGIN_X_RATE = 0
 export const CONNECTOR_INNTER_MARGIN_X = 0
 
 export const ROOT_NODE = 'root'
