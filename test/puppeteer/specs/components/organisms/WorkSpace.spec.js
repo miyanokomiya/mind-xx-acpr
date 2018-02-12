930eaa7cfcfb0a1799595b4354064ba77b1c6e87
miyanokomiya <komiyamatomoya@mbp-2.local>
2018-02-12 09:59:13 +0900

    Add test

--- test/puppeteer/specs/components/organisms/WorkSpace.spec.js
+++ test/puppeteer/specs/components/organisms/WorkSpace.spec.js
@@ -1,9 +1,5 @@
 import puppeteer from 'puppeteer'
-import {
-  getScreenshotFn
-  // inputForm,
-  // getContent
-} from '@/specs/utils'
+import { getScreenshotFn, inputForm, getContent } from '@/specs/utils'
 
 jest.setTimeout(100000)
 const screenshot = getScreenshotFn(
@@ -40,7 +36,7 @@ describe('Map page', () => {
     await browser.close()
   })
 
-  test('Create new file, then delete it', async () => {
+  test('Create new file, edit its name, then delete it', async () => {
     // create new file
     let $createButton = await getCreateButton({ page })
     let $files = await getFiles({ page })
@@ -51,14 +47,46 @@ describe('Map page', () => {
     await screenshot({ page, title: '1.2 after_create' })
     expect($files.length).toBe(fileCount + 1)
 
+    // edit name
+    $files = await getFiles({ page })
+    let $buttons = await getEditButtons({ $file: $files[0] })
+    expect($buttons.length).toBe(3)
+    await $buttons[0].click()
+    await inputForm({
+      page,
+      text: 'edit name',
+      query: '.menuable__content__active input'
+    })
+    await screenshot({ page, title: '1.3 input_name' })
+    await page.keyboard.press('Enter')
+    await page.waitFor(200)
+    $files = await getFiles({ page })
+    let $title = await $files[0].$('.file-name')
+    let name = await getContent({ page, $dom: $title })
+    await screenshot({ page, title: '1.4 edit_name' })
+    expect(name).toBe('edit name')
+
     // delete the file
     $files = await getFiles({ page })
     fileCount = $files.length
-    let $buttons = await getEditButtons({ $file: $files[0] })
+    $buttons = await getEditButtons({ $file: $files[0] })
     await $buttons[2].click()
     await $buttons[2].click()
     $files = await getFiles({ page })
-    await screenshot({ page, title: '1.3 after_delete' })
+    await screenshot({ page, title: '1.5 after_delete' })
     expect($files.length).toBe(fileCount - 1)
+  })
+
+  test('Clone a file', async () => {
+    // clone a file
+    let $files = await getFiles({ page })
+    let fileCount = $files.length
+    let $buttons = await getEditButtons({ $file: $files[0] })
+    await $buttons[1].click()
+    $files = await getFiles({ page })
+    $buttons = await getEditButtons({ $file: $files[0] })
+    await screenshot({ page, title: '2.1 clone_file' })
+    expect($files.length).toBe(fileCount + 1)
+    expect($buttons.length).toBe(3)
   })
 })
--- test/puppeteer/specs/utils.js
+++ test/puppeteer/specs/utils.js
@@ -1,7 +1,20 @@
+import * as fs from 'fs'
+
 const DEFAULT_SCREENSHOT_DIR = 'test/puppeteer/screenshots/tmp/'
 // const INTERVAL = 50
 
 export const getScreenshotFn = (SCREENSHOT_DIR = DEFAULT_SCREENSHOT_DIR) => {
+  // delete the directory
+  if (fs.existsSync(SCREENSHOT_DIR)) {
+    const targetRemoveFiles = fs.readdirSync(SCREENSHOT_DIR)
+    for (let file in targetRemoveFiles) {
+      fs.unlinkSync(SCREENSHOT_DIR + targetRemoveFiles[file])
+    }
+    fs.rmdirSync(SCREENSHOT_DIR)
+  }
+  // create directory
+  fs.mkdirSync(SCREENSHOT_DIR)
+  // return the function to take a screenshot
   return async ({ page, title }) => {
     await page.screenshot({
       path: `${SCREENSHOT_DIR}${title}.png`,
