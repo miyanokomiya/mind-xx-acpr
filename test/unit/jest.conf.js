cd9620a62610e67bd801c371d0b1dfbc353764de
miyanokomiya <komiyamatomoya@mbp-2.local>
2018-04-30 18:07:24 +0900

    作業ログ

--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -2,16 +2,29 @@ const path = require('path')
 
 module.exports = {
   rootDir: path.resolve(__dirname, '../../'),
-  moduleFileExtensions: ['js', 'json', 'vue'],
+  moduleFileExtensions: ['vue', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
+    '^@/(.*)$': '<rootDir>/src/$1',
+    // '\\.(css|scss)$': '<rootDir>/node_modules/jest-css-modules'
+    // '\\.(css|less)$': 'identity-obj-proxy'
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': 'identity-obj-proxy'
   },
   transform: {
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
-    '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
+    // '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
+    '^.+\\.jsx?$': '<rootDir>/node_modules/babel-jest',
+    '.*\\.(vue)$': '<rootDir>/node_modules/jest-vue-preprocessor'
+    // '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+    //   './fileTransformer.js'
   },
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/(?!(@storybook/.*\\.vue$))'
+    // '\\.(css|scss)$'
+  ],
   testPathIgnorePatterns: ['<rootDir>/test/e2e', '<rootDir>/test/puppeteer'],
-  snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
+  // snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
   setupFiles: ['<rootDir>/test/unit/setup'],
   collectCoverage: false,
   coverageDirectory: '<rootDir>/test/unit/coverage',
